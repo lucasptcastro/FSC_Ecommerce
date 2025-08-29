41824e4f2393d4bc5457dc10bdb8d9d15198b8fe
lucasptcastro <lucasptcastro@gmail.com>
2025-08-28 22:23:58 -0300

    feat: add product list and product item components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,45 @@
-import { Header } from "@/components/common/header";
+import Image from "next/image";
 
-export default function Home() {
+import { Header } from "@/components/common/header";
+import { ProductList } from "@/components/common/product-list";
+import { db } from "@/db";
+
+export default async function Home() {
+  const productsWithVariants = await db.query.productTable.findMany({
+    with: {
+      variants: true,
+    },
+  });
+
   return (
     <>
       <Header />
+
+      <div className="space-y-6">
+        <div className="px-5">
+          <Image
+            className="h-auto w-full" // este classname e a propriedade sizes fazem com que a imagem se redimensione conforme o tamanho da tela para manter qualidade
+            src="/banner-01.png"
+            alt="Leve uma vida com estilo"
+            width={0}
+            height={0}
+            sizes="100vw"
+          />
+        </div>
+
+        <ProductList products={productsWithVariants} title="Mais vendidos" />
+
+        <div className="px-5">
+          <Image
+            className="h-auto w-full" // este classname e a propriedade sizes fazem com que a imagem se redimensione conforme o tamanho da tela para manter qualidade
+            src="/banner-02.png"
+            alt="Leve uma vida com estilo"
+            width={0}
+            height={0}
+            sizes="100vw"
+          />
+        </div>
+      </div>
     </>
   );
 }
