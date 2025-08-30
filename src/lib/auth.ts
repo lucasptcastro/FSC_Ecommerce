a6d019a5241b56e703c8bd187868af3e4f56918e
lucasptcastro <lucasptcastro@gmail.com>
2025-08-29 21:10:36 -0300

    feat: add sigin with google

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -13,6 +13,12 @@ export const auth = betterAuth({
   emailAndPassword: {
     enabled: true,
   },
+  socialProviders: {
+    google: {
+      clientId: process.env.GOOGLE_CLIENT_ID as string,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+    },
+  },
   user: {
     modelName: "userTable",
   },
@@ -21,5 +27,8 @@ export const auth = betterAuth({
   },
   account: {
     modelName: "accountTable",
+  },
+  verification: {
+    modelName: "verificationTable",
   },
 });
